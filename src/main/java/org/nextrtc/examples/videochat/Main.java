b5ba4b560dcff35b4b63efac94b2a15469d0f504
Marcin Ślósarz <m.slosarz@ahej.pl>
2018-07-05 22:08:22 +0200

    Ratpack project with sample webapp

--- src/main/java/org/nextrtc/examples/videochat/Main.java
+++ src/main/java/org/nextrtc/examples/videochat/Main.java
@@ -1,7 +1,12 @@
 package org.nextrtc.examples.videochat;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
+
+import java.security.SecureRandom;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -12,6 +17,7 @@ public class Main {
                                         .connectQueueSize(10)
                                         .threads(10)
                                         .baseDir(BaseDir.find())
+                                        .ssl(buildSslContext())
                         )
                         .registryOf(registry -> registry
                                 .add(new HttpHandler())
@@ -19,8 +25,19 @@ public class Main {
                         .handlers(
                                 chain -> chain
                                         .get("signaling", HttpHandler.class)
-                                        .files(f -> f.indexFiles("index.html"))
+                                        .files(f -> f.dir("app")
+                                                .indexFiles("index.html"))
                         )
         );
     }
+
+    private static SslContext buildSslContext() {
+        try {
+            SelfSignedCertificate localhost = new SelfSignedCertificate("localhost", new SecureRandom(), 1024);
+            return SslContextBuilder.forServer(localhost.certificate(), localhost.privateKey()).build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
