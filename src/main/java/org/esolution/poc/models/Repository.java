f789c20cc2d382da470ca75ebebe1b55d0f31aef
ihula <hula.ihor@gmail.com>
2019-03-26 22:20:53 +0200

    Add lombok library

--- src/main/java/org/esolution/poc/models/Repository.java
+++ src/main/java/org/esolution/poc/models/Repository.java
@@ -1,31 +1,12 @@
 package org.esolution.poc.models;
 
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class Repository {
 
     private String name;
-
     private String description;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Repository [name=" + name + ", description=" + description + "]";
-    }
-
 }
