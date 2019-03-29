e1787c3420cd79044cce25f9459939065d0d50d9
igorokgula <hula.ihor@gmail.com>
2019-03-29 06:31:13 +0200

    Get top contributors Test

--- src/main/java/org/esolution/poc/models/Contributor.java
+++ src/main/java/org/esolution/poc/models/Contributor.java
@@ -1,11 +1,15 @@
 package org.esolution.poc.models;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contributor {
 
     @SerializedName("login")
